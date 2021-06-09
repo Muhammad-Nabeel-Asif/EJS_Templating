871bfcc245d31ffaffaf490a46d4e6cd85ecc39b
Nabeel Asif <nabeel.asif362@gmail.com>
2021-06-09 16:55:04 +0500

    ejs modification

--- index.js
+++ index.js
@@ -9,7 +9,18 @@ app.set("views", path.join(__dirname, "/views"));
 
 // sending get request and the rendering the response from server as file : home.ejs
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home");
+});
+
+app.get("/rand", (req, res) => {
+  const num = Math.floor(Math.random() * 10) + 1;
+  // passing object to ejs file
+  res.render("random", { num });
+});
+
+app.get("/r/:subreddit", (req, res) => {
+  const { subreddit } = req.params;
+  res.render("subreddit", { subreddit });
 });
 
 app.listen(3000, () => {
